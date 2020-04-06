5a401cbe44b0ae5ab91a9f5fd57abdacccaf7220
长安 <2648835884@qq.com>
2020-04-06 10:56:13 +0800

    add cookie

--- src/main/java/cn/lxg/community/controller/IndexController.java
+++ src/main/java/cn/lxg/community/controller/IndexController.java
@@ -1,13 +1,33 @@
 package cn.lxg.community.controller;
+import	java.net.Authenticator;
 
+import cn.lxg.community.mapper.UserMapper;
+import cn.lxg.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")) {
+                User user = userMapper.selectOneByToken(cookie.getValue());
+                if(user != null) {
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 
--- src/main/java/cn/lxg/community/mapper/UserMapper.java
+++ src/main/java/cn/lxg/community/mapper/UserMapper.java
@@ -3,11 +3,16 @@ package cn.lxg.community.mapper;
 import cn.lxg.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into user(account_id,name,token,gmt_create,gmt_modified) values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified}) ")
     void insertUser(User user);
+
+    @Select("select * from user where token = #{token}")
+    User selectOneByToken(@Param("token")String token);
 
 }
